5255bc262bc9f19c854d8c2b82e38163f74798e0
Pavel Popov <paavali.popov@gmail.com>
2023-05-22 15:16:22 -0400

    added wandb logger (may add others later)

--- src/logger.py
+++ src/logger.py
@@ -1,22 +1,20 @@
 """Logger factory"""
+from omegaconf import open_dict
 import wandb
 
 
-def logger_factory(conf, model_config):
+def logger_factory(cfg, model_cfg):
     """Basic logger factory"""
     logger = wandb.init(
-        project=conf.project_name,
-        name=conf.wandb_trial_name,
+        project=cfg.project_name,
+        name=cfg.wandb_trial_name,
         save_code=True,
     )
 
     # save tuning process wandb link
-    if conf.mode == "tune":
+    if cfg.exp.mode == "tune":
         link = logger.get_url()
-    else:
-        if "link" in model_config:
-            link = model_config["link"]
-        else:
-            link = None
+        with open_dict(model_cfg):
+            model_cfg.link = link
 
-    return logger, link
+    return logger
