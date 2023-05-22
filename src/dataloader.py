2b3141730cf92b40061eb2cec9078a1f82a7fb11
Pavel Popov <paavali.popov@gmail.com>
2023-05-22 15:14:40 -0400

    added basic dataloader and dataloader factory

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,115 +1,119 @@
-# pylint: disable=no-member, invalid-name, too-many-locals, too-many-arguments
+# pylint: disable=no-member, invalid-name, too-many-locals, too-many-arguments, consider-using-dict-items
 """ Scripts for creating dataloaders """
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from importlib import import_module
 
+from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
 
-def dataloader_factory(conf, data, outer_k, trial=None, inner_k=None):
-    """Return dataloaders according to the used model"""
-    if conf.model in ["lstm", "mean_lstm", "transformer", "mean_transformer", "dice"]:
-        dataloaders = common_dataloader(conf, data, outer_k, trial, inner_k)
+def dataloader_factory(cfg, data, k, trial=None):
+    """Return dataloader according to the used model"""
+    if "custom_dataloader" not in cfg.model or not cfg.model.custom_dataloader:
+        dataloader = common_dataloader(cfg, data, k, trial)
     else:
-        raise ValueError(f"'{conf.model}' model is not recognized")
+        try:
+            model_module = import_module(f"src.models.{cfg.model.name}")
+        except ModuleNotFoundError as e:
+            raise ModuleNotFoundError(
+                f"No module named '{cfg.model.name}' \
+                                    found in 'src.models'. Check if model name \
+                                    in config file and its module name are the same"
+            ) from e
 
-    return dataloaders
+        try:
+            get_dataloader = model_module.get_dataloader
+        except AttributeError as e:
+            raise AttributeError(
+                f"'src.models.{cfg.model.name}' has no function\
+                                'get_dataloader'. Is the function misnamed/not defined?"
+            ) from e
+
+        dataloader = get_dataloader()
+
+    return dataloader
 
 
-def common_dataloader(conf, data, outer_k, trial=None, inner_k=None):
+def common_dataloader(cfg, data, k, trial=None):
     """
-    Most generic dataloaders, returns time-series data and labels,
-    split into train/val/test sets
+    Return common dataloaders
+    dataloaders are a dictionary of
+    {
+        "train": data,
+        "valid": data,
+        "test": data,
+        "any additional test": data,
+    }
     """
-    skf = StratifiedKFold(n_splits=conf.n_splits, shuffle=True, random_state=42)
-    CV_folds = list(skf.split(data["main"]["features"], data["main"]["labels"]))
+    split_data = {"train": {}, "valid": {}, "test": {}}
 
     # train/test split
-    train_index, test_index = CV_folds[outer_k]
-    X_train, X_test = (
-        data["main"]["features"][train_index],
-        data["main"]["features"][test_index],
+    split_data["train"], split_data["test"] = cross_validation_split(
+        data["main"], cfg.exp.n_splits, k
     )
-    y_train, y_test = (
-        data["main"]["labels"][train_index],
-        data["main"]["labels"][test_index],
-    )
-
-    if conf.mode == "introspection":
-        if data["main"]["mask"] is not None:
-            mask = data["main"]["mask"][test_index]
-            mask = torch.tensor(mask, dtype=torch.float32)
-        else:
-            mask = None
-        return {
-            "features": torch.tensor(X_test, dtype=torch.float32),
-            "labels": torch.tensor(y_test, dtype=torch.int64),
-            "mask": mask,
-        }
-
-    if conf.mode == "tune" and not conf.glob:
-        # tune mode should be completely unaware of the test set
-        # unless we are looking for globally optimal hyperparams
-        inner_skf = StratifiedKFold(
-            n_splits=conf.n_splits, shuffle=True, random_state=42
-        )
-        inner_CV_folds = list(inner_skf.split(X_train, y_train))
-
-        train_index, test_index = inner_CV_folds[inner_k]
-        X_train, X_test = X_train[train_index], X_train[test_index]
-        y_train, y_test = y_train[train_index], y_train[test_index]
 
     # train/val split
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train,
-        y_train,
-        test_size=X_train.shape[0] // conf.n_splits,
-        random_state=42 + trial if conf.mode == "exp" else 42,
-        stratify=y_train,
+    splitter = StratifiedShuffleSplit(
+        n_splits=cfg.exp.n_trials,
+        test_size=split_data["train"]["labels"].shape[0] // cfg.exp.n_splits,
+        random_state=42,
     )
+    tr_val_splits = list(
+        splitter.split(split_data["train"]["labels"], split_data["train"]["labels"])
+    )
+    train_index, val_index = (
+        tr_val_splits[0] if cfg.exp.mode == "tune" else tr_val_splits[trial]
+    )
+    for key in split_data["train"]:
+        split_data["train"][key], split_data["valid"][key] = (
+            split_data["train"][key][train_index],
+            split_data["train"][key][val_index],
+        )
+
+    # TODO: add support for extra test datasets
 
     # create dataloaders
     dataloaders = {}
-    dataloaders["train"] = DataLoader(
-        TensorDataset(
-            torch.tensor(X_train, dtype=torch.float32),
-            torch.tensor(y_train, dtype=torch.int64),
-        ),
-        batch_size=conf.batch_size,
-        num_workers=0,
-        shuffle=True,
-    )
-    dataloaders["valid"] = DataLoader(
-        TensorDataset(
-            torch.tensor(X_val, dtype=torch.float32),
-            torch.tensor(y_val, dtype=torch.int64),
-        ),
-        batch_size=conf.batch_size,
-        num_workers=0,
-        shuffle=False,
-    )
-    dataloaders["test"] = DataLoader(
-        TensorDataset(
-            torch.tensor(X_test, dtype=torch.float32),
-            torch.tensor(y_test, dtype=torch.int64),
-        ),
-        batch_size=conf.batch_size,
-        num_workers=0,
-        shuffle=False,
-    )
+    key_order = ["TS", "FNC", "labels"]
+    for key in split_data:
+        for data_key in split_data[key]:
+            if data_key == "labels":
+                split_data[key][data_key] = torch.tensor(
+                    split_data[key][data_key], dtype=torch.int64
+                )
+            else:
+                split_data[key][data_key] = torch.tensor(
+                    split_data[key][data_key], dtype=torch.float32
+                )
+        # order-wise unpacking: 'key_order' order should be followed
+        unpacked_tensors = [
+            split_data[key].get(data_key)
+            for data_key in key_order
+            if data_key in split_data[key]
+        ]
+        assert len(unpacked_tensors) == len(split_data[key])
 
-    # dataloaders for extra test datasets
-    for ds in data:
-        if ds != "main" and conf.mode != "tune":
-            dataloaders[ds] = DataLoader(
-                TensorDataset(
-                    torch.tensor(data[ds]["features"], dtype=torch.float32),
-                    torch.tensor(data[ds]["labels"], dtype=torch.int64),
-                ),
-                batch_size=conf.batch_size,
-                num_workers=0,
-                shuffle=False,
-            )
-    ####
+        dataloaders[key] = DataLoader(
+            TensorDataset(*unpacked_tensors),
+            batch_size=cfg.exp.batch_size,
+            num_workers=0,
+            shuffle=key == "train",
+        )
 
     return dataloaders
+
+
+def cross_validation_split(data, n_splits, k):
+    train_data = {}
+    test_data = {}
+
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+    CV_folds = list(skf.split(data["labels"], data["labels"]))
+    train_index, test_index = CV_folds[k]
+    for key in data:
+        train_data[key], test_data[key] = (
+            data[key][train_index],
+            data[key][test_index],
+        )
+
+    return train_data, test_data
