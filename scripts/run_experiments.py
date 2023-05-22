a5a521dd316529a66c7cd788272bb923b3e62a44
Pavel Popov <paavali.popov@gmail.com>
2023-05-22 15:01:04 -0400

    added script for tuning

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -4,189 +4,276 @@ import sys
 import os
 import json
 
+from omegaconf import DictConfig, OmegaConf
+import hydra
+
 import pandas as pd
 import numpy as np
 
-from src.utils import get_argparser, get_resumed_params, NpEncoder
+from src.utils import set_project_name, set_run_name
 from src.data import data_factory, data_postfactory
-from src.naming import project_name, run_name
-from src.model_config import model_config_factory
+from src.dataloader import dataloader_factory, cross_validation_split
+from src.model import model_config_factory, model_factory
+from src.model_utils import criterion_factory, optimizer_factory, scheduler_factory
 from src.logger import logger_factory
-from src.dataloader import dataloader_factory
-from src.model import model_factory
-from src.optimizer import optimizer_factory
-from src.scheduler import scheduler_factory
-from src.criterion import criterion_factory
 from src.trainer import trainer_factory
 
 
-def start(conf):
-    """Main script for starting the experiments"""
+@hydra.main(version_base=None, config_path="../src/conf", config_name="exp_config")
+def start(cfg):
+    """Main script for starting experiments"""
 
-    # set up wandb environment
-    os.environ["WANDB_SILENT"] = "true" if conf.wandb_silent else "false"
-    os.environ["WANDB_MODE"] = "offline" if conf.wandb_offline else "online"
+    # set wandb environment
+    os.environ["WANDB_SILENT"] = "true" if cfg.wandb_silent else "false"
+    os.environ["WANDB_MODE"] = "offline" if cfg.wandb_offline else "online"
 
-    # get project name and dir, get resumed params if mode is 'resume'
-    conf.project_name, conf.project_dir = project_name(conf)
-    if conf.mode == "resume":
-        assert conf.path is not None
-        conf, start_k, start_trial = get_resumed_params(conf)
-    else:
-        start_k, start_trial = 0, 0
+    # set project name and directory
+    set_project_name(cfg)
+    os.makedirs(cfg.project_dir, exist_ok=True)
+    with open(f"{cfg.project_dir}/general_config.yaml", "w", encoding="utf8") as f:
+        OmegaConf.save(cfg, f)
 
-    # save config
-    os.makedirs(conf.project_dir, exist_ok=True)
-    with open(conf.project_dir + "general_config.json", "w", encoding="utf8") as fp:
-        json.dump(vars(conf), fp, indent=2, cls=NpEncoder)
+    # load dataset, compute FNCs if model requires them.
+    # if exp.single_HP and exp.tuning_holdout set to True,
+    # returns tuning/experimental data depending on exp.mode
+    original_data = data_factory(cfg)
 
-    # load raw dataset
-    original_data, conf.data_info = data_factory(conf)
+    print(OmegaConf.to_yaml(cfg))
 
-    if conf.mode == "tune":
-        # outer CV: for each test set, we are looking for a unique set of optimal hyperparams
-        for outer_k in range(start_k, conf.n_splits):
-            # num trials: number of hyperparams sets to test
-            if outer_k != start_k:
-                start_trial = 0
-            for trial in range(start_trial, conf.n_trials):
-                model_config = model_config_factory(conf)
-                # some models require data postprocessing (based on their config)
-                data, conf.data_info = data_postfactory(
-                    conf,
-                    model_config,
-                    original_data,
+    if cfg.exp.mode == "tune":
+        if cfg.exp.single_HP:
+            # use the whole original_data for tuning.
+            # the obtained HPs are better to be used on other datasets
+            # (unless exp.tuning_holdout is True),
+            # otherwise there is a testing-on-train-data danger
+            tune(cfg=cfg, original_data=original_data)
+
+        else:
+            # tune each original_data CV fold independently
+            for k in range(0, cfg.exp.n_splits):
+                tune_fold_data = original_data
+                tune_fold_data["main"], _ = cross_validation_split(
+                    tune_fold_data["main"], cfg.exp.n_splits, k
                 )
-                model_config["data_info"] = conf.data_info
+                tune(cfg=cfg, original_data=tune_fold_data, outer_k=k)
 
-                # inner CV: CV of the chosen hyperparams
-                for inner_k in range(conf.n_splits):
-                    print(
-                        f"Running tune: k: {outer_k:02d}, Trial: {trial:03d}, \
-                            Inner k: {inner_k:02d},"
-                    )
+    # if cfg.exp.mode == "tune":
+    #     # outer CV: for each test set, we are looking for a unique set of optimal hyperparams
+    #     for outer_k in range(0, cfg.exp.n_splits):
+    #         # num trials: number of hyperparams sets to test
+    #         for trial in range(0, cfg.exp.n_trials):
+    #             model_config = model_config_factory(cfg)
+    #             # some models require data postprocessing (based on their config)
+    #             data, conf.data_info = data_postfactory(
+    #                 cfg,
+    #                 model_config,
+    #                 original_data,
+    #             )
+    #             model_config["data_info"] = conf.data_info
 
-                    (
-                        conf.wandb_trial_name,
-                        conf.outer_k_dir,
-                        conf.trial_dir,
-                        conf.run_dir,
-                    ) = run_name(conf, outer_k, trial, inner_k)
-                    os.makedirs(conf.run_dir, exist_ok=True)
+    #             set_run_name(cfg, outer_k, trial, inner_k)
+    #             os.makedirs(conf.run_dir, exist_ok=True)
 
-                    dataloaders = dataloader_factory(
-                        conf, data, outer_k, trial, inner_k
-                    )
-                    model = model_factory(conf, model_config)
-                    criterion = criterion_factory(conf, model_config)
-                    optimizer = optimizer_factory(conf, model, model_config)
-                    scheduler = scheduler_factory(conf, optimizer, model_config)
+    #             ###########
 
-                    logger, model_config["link"] = logger_factory(conf, model_config)
+    #             # inner CV: CV of the chosen hyperparams
+    #             for inner_k in range(conf.n_splits):
+    #                 print(
+    #                     f"Running tune: k: {outer_k:02d}, Trial: {trial:03d}, \
+    #                         Inner k: {inner_k:02d},"
+    #                 )
 
-                    trainer = trainer_factory(
-                        conf,
-                        model_config,
-                        dataloaders,
-                        model,
-                        criterion,
-                        optimizer,
-                        scheduler,
-                        logger,
-                    )
-                    results = trainer.run()
-                    # save results of nested CV
-                    df = pd.DataFrame(results, index=[0])
-                    with open(conf.trial_dir + "runs.csv", "a", encoding="utf8") as f:
-                        df.to_csv(f, header=f.tell() == 0, index=False)
+    #                 (
+    #                     conf.wandb_trial_name,
+    #                     conf.outer_k_dir,
+    #                     conf.trial_dir,
+    #                     conf.run_dir,
+    #                 ) = run_name(conf, outer_k, trial, inner_k)
+    #                 os.makedirs(conf.run_dir, exist_ok=True)
 
-                    logger.finish()
+    #                 dataloaders = dataloader_factory(
+    #                     conf, data, outer_k, trial, inner_k
+    #                 )
+    #                 model = model_factory(conf, model_config)
+    #                 criterion = criterion_factory(conf, model_config)
+    #                 optimizer = optimizer_factory(conf, model, model_config)
+    #                 scheduler = scheduler_factory(conf, optimizer, model_config)
 
-                # save model config
-                with open(
-                    conf.trial_dir + "model_config.json", "w", encoding="utf8"
-                ) as fp:
-                    json.dump(model_config, fp, indent=2, cls=NpEncoder)
-                # read inner CV results, save the average in the fold dir
-                df = pd.read_csv(conf.trial_dir + "runs.csv")
-                score = np.mean(df["test_score"].to_numpy())
-                loss = np.mean(df["test_average_loss"].to_numpy())
-                df = pd.DataFrame(
-                    {
-                        "trial": trial,
-                        "score": score,
-                        "loss": loss,
-                        "path_to_config": conf.trial_dir + "model_config.json",
-                    },
-                    index=[0],
-                )
-                with open(conf.outer_k_dir + "runs.csv", "a", encoding="utf8") as f:
-                    df.to_csv(f, header=f.tell() == 0, index=False)
+    #                 logger, model_config["link"] = logger_factory(conf, model_config)
 
-    elif conf.mode == "exp":
-        # outer CV: for each test set, we are loading a unique set of optimal hyperparams
-        for outer_k in range(start_k, conf.n_splits):
-            # loading best config requires project_name
-            model_config = model_config_factory(conf, k=outer_k)
+    #                 trainer = trainer_factory(
+    #                     conf,
+    #                     model_config,
+    #                     dataloaders,
+    #                     model,
+    #                     criterion,
+    #                     optimizer,
+    #                     scheduler,
+    #                     logger,
+    #                 )
+    #                 results = trainer.run()
+    #                 # save results of nested CV
+    #                 df = pd.DataFrame(results, index=[0])
+    #                 with open(conf.trial_dir + "runs.csv", "a", encoding="utf8") as f:
+    #                     df.to_csv(f, header=f.tell() == 0, index=False)
 
-            # some models require data postprocessing (based on their config)
-            data, conf.data_info = data_postfactory(
-                conf,
-                model_config,
-                original_data,
-            )
+    #                 logger.finish()
 
-            # num trials: for each test set, we are splitting training set into train/val randomly
-            if outer_k != start_k:
-                start_trial = 0
-            for trial in range(start_trial, conf.n_trials):
-                print(f"Running exp: k: {outer_k:02d}, Trial: {trial:03d}")
+    #             # save model config
+    #             with open(
+    #                 conf.trial_dir + "model_config.json", "w", encoding="utf8"
+    #             ) as fp:
+    #                 json.dump(model_config, fp, indent=2, cls=NpEncoder)
+    #             # read inner CV results, save the average in the fold dir
+    #             df = pd.read_csv(conf.trial_dir + "runs.csv")
+    #             score = np.mean(df["test_score"].to_numpy())
+    #             loss = np.mean(df["test_average_loss"].to_numpy())
+    #             df = pd.DataFrame(
+    #                 {
+    #                     "trial": trial,
+    #                     "score": score,
+    #                     "loss": loss,
+    #                     "path_to_config": conf.trial_dir + "model_config.json",
+    #                 },
+    #                 index=[0],
+    #             )
+    #             with open(conf.outer_k_dir + "runs.csv", "a", encoding="utf8") as f:
+    #                 df.to_csv(f, header=f.tell() == 0, index=False)
 
-                (conf.wandb_trial_name, conf.outer_k_dir, conf.run_dir) = run_name(
-                    conf, outer_k, trial
-                )
-                os.makedirs(conf.run_dir, exist_ok=True)
-                with open(
-                    conf.outer_k_dir + "model_config.json", "w", encoding="utf8"
-                ) as fp:
-                    json.dump(model_config, fp, indent=2, cls=NpEncoder)
+    # elif conf.mode == "exp":
+    #     # outer CV: for each test set, we are loading a unique set of optimal hyperparams
+    #     for outer_k in range(start_k, conf.n_splits):
+    #         # loading best config requires project_name
+    #         model_config = model_config_factory(conf, k=outer_k)
 
-                dataloaders = dataloader_factory(conf, data, outer_k, trial)
-                model = model_factory(conf, model_config)
-                criterion = criterion_factory(conf, model_config)
-                optimizer = optimizer_factory(conf, model, model_config)
-                scheduler = scheduler_factory(conf, optimizer, model_config)
+    #         # some models require data postprocessing (based on their config)
+    #         data, conf.data_info = data_postfactory(
+    #             conf,
+    #             model_config,
+    #             original_data,
+    #         )
 
-                logger, model_config["link"] = logger_factory(conf, model_config)
+    #         # num trials: for each test set, we are splitting training set into train/val randomly
+    #         if outer_k != start_k:
+    #             start_trial = 0
+    #         for trial in range(start_trial, conf.n_trials):
+    #             print(f"Running exp: k: {outer_k:02d}, Trial: {trial:03d}")
 
-                trainer = trainer_factory(
-                    conf,
-                    model_config,
-                    dataloaders,
-                    model,
-                    criterion,
-                    optimizer,
-                    scheduler,
-                    logger,
-                )
-                results = trainer.run()
-                # save results of the trial
-                df = pd.DataFrame(results, index=[0])
-                with open(conf.project_dir + "runs.csv", "a", encoding="utf8") as f:
-                    df.to_csv(f, header=f.tell() == 0, index=False)
+    #             (conf.wandb_trial_name, conf.outer_k_dir, conf.run_dir) = run_name(
+    #                 conf, outer_k, trial
+    #             )
+    #             os.makedirs(conf.run_dir, exist_ok=True)
+    #             with open(
+    #                 conf.outer_k_dir + "model_config.json", "w", encoding="utf8"
+    #             ) as fp:
+    #                 json.dump(model_config, fp, indent=2, cls=NpEncoder)
 
-                logger.finish()
+    #             dataloaders = dataloader_factory(conf, data, outer_k, trial)
+    #             model = model_factory(conf, model_config)
+    #             criterion = criterion_factory(conf, model_config)
+    #             optimizer = optimizer_factory(conf, model, model_config)
+    #
 
-    else:
-        raise ValueError(f"{conf.model} is not recognized")
+    #             logger, model_config["link"] = logger_factory(conf, model_config)
+
+    #             trainer = trainer_factory(
+    #                 conf,
+    #                 model_config,
+    #                 dataloaders,
+    #                 model,
+    #                 criterion,
+    #                 optimizer,
+    #                 scheduler,
+    #                 logger,
+    #             )
+    #             results = trainer.run()
+    #             # save results of the trial
+    #             df = pd.DataFrame(results, index=[0])
+    #             with open(conf.project_dir + "runs.csv", "a", encoding="utf8") as f:
+    #                 df.to_csv(f, header=f.tell() == 0, index=False)
+
+    #             logger.finish()
+
+    # else:
+    #     raise ValueError(f"{conf.model} is not recognized")
+
+
+def tune(cfg, original_data, outer_k=None):
+    # for each trial get new set of HPs, test them using CV
+    for trial in range(0, cfg.exp.n_trials):
+        # get random model config
+        model_cfg = model_config_factory(cfg)
+        # reshape data according to model config (if needed)
+        data = data_postfactory(
+            cfg,
+            model_cfg,
+            original_data,
+        )
+        # run nested CV
+        for inner_k in range(0, cfg.exp.n_splits):
+            set_run_name(cfg, outer_k=outer_k, trial=trial, inner_k=inner_k)
+            os.makedirs(cfg.run_dir, exist_ok=True)
+            dataloaders = dataloader_factory(cfg, data, k=inner_k)
+            results = run_trial(cfg, model_cfg, dataloaders)
+
+            # save results of nested CV in the trial directory
+            df = pd.DataFrame(results, index=[0])
+            with open(f"{cfg.trial_dir}/CV_runs.csv", "a", encoding="utf8") as f:
+                df.to_csv(f, header=f.tell() == 0, index=False)
+
+        # save model config
+        with open(f"{cfg.trial_dir}/model_config.yaml", "w", encoding="utf8") as f:
+            OmegaConf.save(model_cfg, f)
+
+        # summarize the trial's CV results and save them
+        df = pd.read_csv(f"{cfg.trial_dir}/CV_runs.csv")
+        score = np.mean(df["test_score"].to_numpy())
+        loss = np.mean(df["test_average_loss"].to_numpy())
+        df = pd.DataFrame(
+            {
+                "trial": trial,
+                "score": score,
+                "loss": loss,
+                "path_to_config": f"{cfg.trial_dir}/model_config.yaml",
+            },
+            index=[0],
+        )
+        with open(f"{cfg.k_dir}/trial_runs.csv", "a", encoding="utf8") as f:
+            df.to_csv(f, header=f.tell() == 0, index=False)
+
+    # get optimal config and save it
+    df = pd.read_csv(f"{cfg.k_dir}/trial_runs.csv")
+    best_idx = df["score"].idxmax()
+    best_config_path = df.loc[best_idx]["path_to_config"]
+    with open(best_config_path, "r", encoding="utf8") as f:
+        best_config = OmegaConf.load(f)
+    with open(f"{cfg.k_dir}/best_config.yaml", "w", encoding="utf8") as f:
+        OmegaConf.save(best_config, f)
+
+
+def run_trial(cfg, model_cfg, dataloaders):
+    model = model_factory(cfg, model_cfg)
+    criterion = criterion_factory(cfg, model_cfg)
+    optimizer = optimizer_factory(cfg, model, model_cfg)
+    scheduler = scheduler_factory(cfg, optimizer, model_cfg)
+    logger = logger_factory(cfg, model_cfg)
+
+    trainer = trainer_factory(
+        cfg,
+        model_cfg,
+        dataloaders,
+        model,
+        criterion,
+        optimizer,
+        scheduler,
+        logger,
+    )
+
+    results = trainer.run()
+    logger.finish()
+
+    return results
 
 
 if __name__ == "__main__":
-    import warnings
-
-    warnings.filterwarnings("ignore")
-
-    parser = get_argparser(sys.argv)
-    args = parser.parse_args()
-
-    start(args)
+    start()
