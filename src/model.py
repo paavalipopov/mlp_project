2941aa653762b782ed46811146f6af7a00a68b4c
Pavel Popov <paavali.popov@gmail.com>
2023-05-23 14:53:12 -0400

    new config updates and best config loader

--- src/model.py
+++ src/model.py
@@ -2,14 +2,18 @@
 """Models for experiments and functions for setting them up"""
 
 from importlib import import_module
-from omegaconf import OmegaConf
+import os
+
+from omegaconf import OmegaConf, open_dict
+
+from src.settings import LOGS_ROOT
 
 
 def model_config_factory(cfg, k=None):
     """Model config factory"""
-    if cfg.exp.mode == "tune":
+    if cfg.mode.name == "tune":
         model_config = get_tune_config(cfg)
-    elif cfg.exp.mode == "exp":
+    elif cfg.mode.name == "exp":
         model_config = get_best_config(cfg, k)
     else:
         raise NotImplementedError
@@ -18,6 +22,10 @@ def model_config_factory(cfg, k=None):
 
 
 def get_tune_config(cfg):
+    """Returns random HPs defined by the models random_HPs() function"""
+    if "tunable" in cfg.model:
+        assert cfg.model.tunable, "Model is specified as not tunable, aborting"
+
     try:
         model_module = import_module(f"src.models.{cfg.model.name}")
     except ModuleNotFoundError as e:
@@ -36,10 +44,7 @@ def get_tune_config(cfg):
                              tuned, or the function misnamed/not defined?"
         ) from e
 
-    try:
-        model_cfg = random_HPs(cfg)
-    except TypeError:
-        model_cfg = random_HPs()
+    model_cfg = random_HPs(cfg)
 
     print("Tuning model config:")
     print(f"{OmegaConf.to_yaml(model_cfg)}")
@@ -47,57 +52,94 @@ def get_tune_config(cfg):
     return model_cfg
 
 
-def get_best_config(cfg, k):
-    pass
+def get_best_config(cfg, k=None):
+    """
+    1. If cfg.single_HP is True, return the HPs stored in cfg.model_cfg_path. You should avoid using it.
+    2. If cfg.model.default_HP is True, return the HPs defined by 'default_HPs(cfg)' function in the model's .py module
+    3. If the above is false, returns the optimal HPs of the given model for the given k,
+        or, if cfg.dataset.tuning_holdout is True, a single optimal set of HPs
+    """
+    if "single_HPs" in cfg and cfg.single_HPs:
+        # 1. try to load config from cfg.model_cfg_path.
+        # Note: dataset shape information is loaded as 'data_info' key,
+        # just in case
+        assert cfg.model_cfg_path.endswith(
+            ".yaml", ".json"
+        ), f"'{cfg.model_cfg_path}' \
+            is not json or yaml file, aborting"
+        try:
+            with open(cfg.model_cfg_path, "r", encoding="utf8") as f:
+                model_cfg = OmegaConf.load(f)
+        except FileNotFoundError as e:
+            raise FileNotFoundError(
+                f"'{cfg.model_cfg_path}' config is not found"
+            ) from e
+
+        with open_dict(model_cfg):
+            model_cfg.data_info = cfg.dataset.data_info
+
+    elif cfg.model.default_HP:
+        # 2. try to get the HPs defined by 'default_HPs(cfg)' function in the model's .py module
+        try:
+            model_module = import_module(f"src.models.{cfg.model.name}")
+        except ModuleNotFoundError as e:
+            raise ModuleNotFoundError(
+                f"No module named '{cfg.model.name}' \
+                                    found in 'src.models'. Check if model name \
+                                    in config file and its module name are the same"
+            ) from e
+
+        try:
+            default_HPs = model_module.default_HPs
+        except AttributeError as e:
+            raise AttributeError(
+                f"'src.models.{cfg.model.name}' has no function\
+                                'default_HPs'. Is the function misnamed/not defined?"
+            ) from e
+
+        model_cfg = default_HPs(cfg)
+
+    else:
+        # 3. load the optimal HPs from logs
+        if "tuning_holdout" in cfg.dataset and cfg.dataset.tuning_holdout:
+            assert k is not None
+
+        searched_dir = cfg.project_name.split("-")
+        if cfg.used_default_prefix:
+            searched_dir = "tune-" + "-".join(searched_dir[2:])
+        else:
+            searched_dir = f"{cfg.prefix}-tune-" + "-".join(searched_dir[2:])
+
+        print(f"Searching trained model in '{LOGS_ROOT}/*{searched_dir}'")
+        dirs = []
+        for logdir in os.listdir(LOGS_ROOT):
+            if logdir.endswith(searched_dir):
+                dirs.append(os.path.join(LOGS_ROOT, logdir))
+
+        assert (
+            len(dirs) != 0
+        ), "No matching directory found. \
+            Did you set wrong project prefix, or did not run HP tuning? \
+                If you intended to load default HPs, you should \
+                      set 'default_HP' to True in cfg.model"
+        # if multiple run files found, choose the latest
+        found_dir = sorted(dirs)[-1]
+        print(f"Using best model from {found_dir}/k_{k:02d}/")
+
+        # get model config
+        if "tuning_holdout" in cfg.dataset and cfg.dataset.tuning_holdout:
+            best_config_path = f"{found_dir}/best_config.yaml"
+        else:
+            best_config_path = f"{found_dir}/k_{k:02d}/best_config.yaml"
+        with open(best_config_path, "r", encoding="utf8") as f:
+            model_cfg = OmegaConf.load(f)
+
+    print("Loaded model config:")
+    print(model_cfg)
+    return model_cfg
 
 
-# TODO: implement
-# if conf.glob:
-#     pass
-#     # with open(f"{HYPERPARAMS_ROOT}/{conf.model}.json", "r", encoding="utf8") as fp:
-#     #     model_config = json.load(fp)
-#     #     model_config["input_size"] = conf.data_info["data_shape"]["main"][2]
-#     #     model_config["output_size"] = conf.data_info["n_classes"]
-
-# else:
-#     assert k is not None
-
-#     model_config = {}
-
-#     # find and load the best tuned model
-#     exp_dirs = []
-
-#     searched_dir = conf.project_name.split("-")
-#     searched_dir = "-".join(searched_dir[2:4])
-#     searched_dir = f"tune-{searched_dir}"
-#     if conf.prefix != conf.default_prefix:
-#         searched_dir = f"{conf.prefix}-{searched_dir}"
-#     print(f"Searching trained model in {LOGS_ROOT}/*{searched_dir}")
-#     for logdir in os.listdir(LOGS_ROOT):
-#         if logdir.endswith(searched_dir):
-#             exp_dirs.append(os.path.join(LOGS_ROOT, logdir))
-
-#     # if multiple run files found, choose the latest
-#     exp_dir = sorted(exp_dirs)[-1]
-#     print(f"Using best model from {exp_dir}")
-
-#     # get model config
-#     df = pd.read_csv(
-#         f"{exp_dir}/k_{k:02d}/runs.csv", delimiter=",", index_col=False
-#     )
-#     # pick hyperparams of a model with the highest test_score
-#     best_config_path = df.loc[df["score"].idxmax()].to_dict()
-#     best_config_path = best_config_path["path_to_config"]
-#     with open(best_config_path, "r", encoding="utf8") as fp:
-#         model_config = json.load(fp)
-
-# print("Loaded model config:")
-# print(model_config)
-
-# return model_config
-
-
-def model_factory(cfg, model_cfg=None):
+def model_factory(cfg, model_cfg):
     """Models factory"""
     try:
         model_module = import_module(f"src.models.{cfg.model.name}")
@@ -116,9 +158,6 @@ def model_factory(cfg, model_cfg=None):
                              'get_model'. Is the function misnamed/not defined?"
         ) from e
 
-    try:
-        model = get_model(model_cfg)
-    except TypeError:
-        model = get_model()
+    model = get_model(model_cfg)
 
     return model
