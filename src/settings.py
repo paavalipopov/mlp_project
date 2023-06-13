181a9ec366ab735cbd5d0d809763de74cc74fb51
Pavel Popov <kapssula@gmail.com>
2023-06-13 23:20:52 +0000

    changed data root for cluster nodes

--- src/settings.py
+++ src/settings.py
@@ -1,20 +1,18 @@
 """Constants of the project"""
 from datetime import datetime
 import os
+import platform
 
 import path
 
 PROJECT_ROOT = path.Path(os.path.dirname(__file__)).joinpath("..").abspath()
 ASSETS_ROOT = PROJECT_ROOT.joinpath("assets")
-
-DATA_ROOT = ASSETS_ROOT.joinpath("data")
 LOGS_ROOT = ASSETS_ROOT.joinpath("logs")
 
 UTCNOW = datetime.utcnow().strftime("%y%m%d.%H%M%S")
 
-MODELS = [
-    "mlp",
-]
-DATASETS = [
-    "fbirn",
-]
+node = platform.node()
+if "arctrd" in node:
+    DATA_ROOT = "/data/users2/ppopov1/datasets"
+else:
+    DATA_ROOT = ASSETS_ROOT.joinpath("data")
